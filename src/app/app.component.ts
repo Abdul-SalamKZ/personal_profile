c2319852533b466985f6f8287af96ecdb034cd36
Abdul Salam <a.salamkz11@gmail.com>
2024-02-27 22:20:05 +0500

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent as SidebarComponent } from "./shared/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SidebarComponent]
 })
 export class AppComponent {
   title = 'persnal_profile';
---
+++ src/app/shared/void-button/void-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-void-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './void-button.component.html',
+  styleUrl: './void-button.component.scss'
+})
+export class VoidButtonComponent {
+
+}
