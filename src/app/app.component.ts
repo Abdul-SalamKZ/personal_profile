eeeefaf783962270cb78a44bff119601fb0fd6b7
Abdul Salam <a.salamkz11@gmail.com>
2024-02-28 22:57:36 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent as SidebarComponent } from "./shared/sidebar/sidebar.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SidebarComponent]
+    imports: [RouterOutlet, FooterComponent, HomeComponent, SidebarComponent]
 })
 export class AppComponent {
   title = 'persnal_profile';
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
+  a= faCoffee;
 })
 export class SidebarComponent {
-
+  faCoffee = faCoffee;
 }
