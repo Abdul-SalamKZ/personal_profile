9ee8e5a2323e27033a9e821dde429c8487886e94
Abdul Salam <a.salamkz11@gmail.com>
2024-03-06 20:28:03 +0500

    skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { AboutComponent } from './shared/about/about.component';
-import { IntroductionComponent } from './shared/introduction/introduction.component';
-import { ResumeComponent } from './shared/resume/resume.component';
-import { ServicesComponent } from './shared/services/services.component';
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { AboutComponent } from "./shared/about/about.component";
+import { IntroductionComponent } from "./shared/introduction/introduction.component";
+import { ResumeComponent } from "./shared/resume/resume.component";
+import { ServicesComponent } from "./shared/services/services.component";
+import { SkillsComponent } from './shared/skills/skills.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [
-    RouterOutlet,
-    HomeComponent,
-    SidebarComponent,
-    AboutComponent,
-    IntroductionComponent,
-    ResumeComponent,
-    ServicesComponent,
-  ],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent, SidebarComponent ,AboutComponent , IntroductionComponent , ResumeComponent , ServicesComponent ,SkillsComponent]
 })
 export class AppComponent {
   title = 'persnal_profile';
