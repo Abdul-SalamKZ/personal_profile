a70b9a36766ec95d26170a179cfb0bb6111354d6
Abdul Salam <a.salamkz11@gmail.com>
2024-03-08 22:06:20 +0500

    code push

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { VoidButtonComponent } from "../shared/void-button/void-button.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [VoidButtonComponent]
 })
 export class HomeComponent {
 
