0b9e65ec1c32f5cd81a4f9d25d647b24ff00b6bf
Abdulsalam <a.salamkz11@gmail.com>
2024-04-16 21:05:58 +0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { ResumeComponent } from "./shared/resume/resume.component";
 import { ServicesComponent } from "./shared/services/services.component";
 import { SkillsComponent } from './shared/skills/skills.component';
 import { ContactComponent } from './shared/contact/contact.component';
+import { MenubarComponent } from "./shared/menubar/menubar.component";
 
 
 @Component({
@@ -15,7 +16,7 @@ import { ContactComponent } from './shared/contact/contact.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HomeComponent, SidebarComponent ,AboutComponent , IntroductionComponent , ResumeComponent , ServicesComponent ,SkillsComponent ,ContactComponent]
+    imports: [RouterOutlet, HomeComponent, SidebarComponent, AboutComponent, IntroductionComponent, ResumeComponent, ServicesComponent, SkillsComponent, ContactComponent, MenubarComponent]
 })
 export class AppComponent {
   title = 'persnal_profile';
